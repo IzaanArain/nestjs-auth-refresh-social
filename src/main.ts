c346b64cc6b4b3836c83c4006e70bdc30ae6837b
IzaanArain <izaansaquib@gmail.com>
2025-05-23 18:42:32 +0500

    feat: enable CORS and update default port to 3001

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    origin: '*', 
+    credentials: true, // if you're using cookies or auth headers
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser()); // all cookies will be parsed on incoming request & set on the request object
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
